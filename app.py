6e274722425fede1d2b055ae20f458aae7ea547f
Alexander Mollohan <ajmollohan@gmail.com>
2020-09-09 21:56:14 -0400

    updating to put auth0 in

--- app.py
+++ app.py
@@ -10,8 +10,54 @@ from flask_migrate import Migrate
 import base64
 import functools
 
+import middleware
+
 
 app = Flask(__name__)
+
+# oauth = OAuth(app)
+
+# # TODO: login in its own file
+# auth0 = oauth.register(
+#     'auth0',
+#     client_id='qt7n6UnMAwIUvN0bFaxkCAPiNFbqXNPQ',
+#     client_secret='YOUR_CLIENT_SECRET',
+#     api_base_url='https://dev-ogr-2kjg.us.auth0.com',
+#     access_token_url='https://dev-ogr-2kjg.us.auth0.com/oauth/token',
+#     authorize_url='https://dev-ogr-2kjg.us.auth0.com/authorize',
+#     client_kwargs={
+#         'scope': 'openid profile email',
+#     },
+# )
+
+# Here we're using the /callback route.
+# @app.route('/callback')
+# def callback_handling():
+#     # Handles response from token endpoint
+#     auth0.authorize_access_token()
+#     resp = auth0.get('userinfo')
+#     userinfo = resp.json()
+
+#     # Store the user information in flask session.
+#     session['jwt_payload'] = userinfo
+#     session['profile'] = {
+#         'user_id': userinfo['sub'],
+#         'name': userinfo['name'],
+#         'picture': userinfo['picture']
+#     }
+#     return redirect('/dashboard')
+
+# @app.route('/login')
+# def login():
+#     return auth0.authorize_redirect(redirect_uri='YOUR_CALLBACK_URL')
+
+# @app.route('/logout')
+# def logout():
+#     # Clear session stored data
+#     session.clear()
+#     # Redirect user to logout endpoint
+#     params = {'returnTo': url_for('home', _external=True), 'client_id': 'qt7n6UnMAwIUvN0bFaxkCAPiNFbqXNPQ'}
+#     return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
 
 # TODO: db initialization and access into discrete file
 POSTGRES = {
@@ -25,58 +71,88 @@ POSTGRES = {
 app.config['DEBUG'] = True
 
 # Db initialization
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# class UsersModel(db.Model):
+#     """Model for the users table"""
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String())
+#     pass_phrase = db.Column(db.String())
+#     created_date = db.Column(db.DateTime, default=datetime.utcnow)
 
 class ImageModel(db.Model):
     """Model for the images table"""
     __tablename__ = 'images'
 
     id = db.Column(db.Integer, primary_key = True)
-    directory = db.Column(db.String())
-    name = db.Column(db.String())
+    file_path = db.Column(db.String())
+    file_name = db.Column(db.String())
+    # users_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    creator_email = db.Column(db.String(), index=True)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __init__(self, name, directory):
-        self.name = name
-        self.directory = directory
+    def __init__(self, file_name, file_path, creator_email):
+        self.file_name = file_name
+        self.file_path = file_path
+        self.creator_email = creator_email
+
+@app.errorhandler(middleware.AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
 
 @app.route("/get-image/<id>", methods=["GET"])
 def get_image(id):
-    # print(id)
+    print(id)
     id = int(id)
 
     if id == -1:
+        print('hola mundo')
         result = ImageModel.query.first()
+        print(result)
+
         all_rows = ImageModel.query.all()
         max_image_id = len(all_rows)
         header_list = "image_id,image_name,max_image_id"
     else:
         result = ImageModel.query.get(id)
-        header_list = "image_id,image_name"
-
-    file_location = os.path.join(result.directory, result.name)
-
+        header_list = "image_id,image_name" 
+    try:
+        file_location = os.path.join(result.file_path, result.file_name)
+    except TypeError:
+        return middleware.build_actual_response(jsonify('no data'))
+    
     response = send_file(file_location, mimetype='image/png')
     response.headers.add("Access-Control-Expose-Headers", header_list)
     response.headers['image_id'] = result.id
-    response.headers['image_name'] = result.name
+    response.headers['image_name'] = result.file_name
     if id == -1:
         response.headers['max_image_id'] = max_image_id
-    
-    return build_actual_response(response)
+    return middleware.build_actual_response(response)
 
 
 # Preview and upload are essentially the same for the moment, need to clarify this terminology
 @app.route("/preview-image", methods=["POST"])
 def preview_image():
     if request.method == 'OPTIONS':
-        return build_preflight_response()
+        return middleware.build_preflight_response()
     elif request.method == 'POST': 
+        email = request.form['email']
+        print(f'email in preview image {email}')
+        if email is None:
+            response = make_response()
+            response.status_code = 401
+            return middleware.build_actual_response(response)
+
         file = request.files['image'] 
-        file_location = add_effect_to_image(file)
-        return build_actual_response(send_file(file_location, mimetype='image/png'))
+        file_location = add_effect_to_image(file, email)
+        return middleware.build_actual_response(send_file(file_location, mimetype='image/png'))
     return 'This should not be hit?'
 
 def handle_directory_for_day():
@@ -90,7 +166,7 @@ def handle_directory_for_day():
             return False
     return today
 
-def add_effect_to_image(file):
+def add_effect_to_image(file, email):
     today = handle_directory_for_day()
     if today is False:
         return 'error creating storage for this image'
@@ -102,15 +178,15 @@ def add_effect_to_image(file):
     file_name = f'{image_name}.png'
     file_path = os.path.join(today, file_name)
 
-    Image.fromarray(changed_image).save(file_path)
+    if os.path.exists(file_path) is False:
+        record_saved_image_location(file_name, today, email)
 
-    record_saved_image_location(file_name, today)
-    
+    Image.fromarray(changed_image).save(file_path)
     return file_path
 
 # Store in db for easy usage
-def record_saved_image_location(file_name, directory):
-    new_image = ImageModel(name=file_name, directory=directory)
+def record_saved_image_location(file_name, file_path, creator_email):
+    new_image = ImageModel(file_name=file_name, file_path=file_path, creator_email=creator_email)
     db.session.add(new_image)
     db.session.commit()
 
@@ -132,20 +208,6 @@ def make_image_circular(file):
     npImage = np.dstack((npImage,npAlpha))
     return npImage
 
-
-# middleware methods for access control; will probably want these in their own file
-def build_preflight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
-    return response
-
-
-# todo: turn into a decorator
-def build_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
 
 # Helper function which displays file fields to viewer
 def print_file_contents(file):
